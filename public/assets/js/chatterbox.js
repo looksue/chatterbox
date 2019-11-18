1d74fbd15a6411b1b547563002ae6f1b57c4f7ac
Sue Look <suelook1@gmail.com>
2019-11-17 19:57:02 -0500

    event listeners added

---
+++ public/assets/js/chatterbox.js
@@ -0,0 +1,67 @@
+// get the questions
+$.getJSON("/questions", function(data) {
+    // do this for each
+    for (var i = 0; i < data.length; i++) {
+      // append the question on the page
+      $("#questions").append(
+        "<li data-id='" +
+          data[i]._id +
+          "'>" +
+          data[i].title +
+          "<br />" +
+          data[i].link +
+          "</li>" +
+          "<br />" +
+          "<br />"
+      );
+    }
+  });
+  
+  // make event listener for list elements
+  $(document).on("click", "li", function() {
+    $("#notes").empty();
+    var thisId = $(this).attr("data-id");
+  
+    // get the article
+    $.ajax({
+      method: "GET",
+      url: "/questions/" + thisId
+    })
+      // print the note(s)
+      .then(function(data) {
+        console.log(data);
+        $("#notes").append("<h2>" + data.title + "</h2>");
+        $("#notes").append("<input id='titleinput' name='title' >");
+        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+        $("#notes").append(
+          "<button data-id='" + data._id + "' id='savenote'>Save Note</button>"
+        );
+  
+        // If there's a note already
+        if (data.note) {
+          $("#titleinput").val(data.note.title);
+          $("#bodyinput").val(data.note.body);
+        }
+      });
+  });
+  
+  // event listener for the save note button
+  $(document).on("click", "#savenote", function() {
+    var thisId = $(this).attr("data-id");
+  
+    $.ajax({
+      method: "POST",
+      url: "/questions/" + thisId,
+      data: {
+        title: $("#titleinput").val(),
+        body: $("#bodyinput").val()
+      }
+    }).then(function(data) {
+      console.log(data);
+      $("#notes").empty();
+    });
+  
+    // clear out the note fields
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
+  });
